605c312b15bd22b9b6ef28bd2e0699775d495bbb
At1aNt7 <At1aNt2007@yandex.ru>
2018-10-26 18:30:55 +0700

    Изи полностью сделал, нормал не сделал 4ое задание, хард не сделал.

--- lesson_03/home_work/hw03_easy.py
+++ lesson_03/home_work/hw03_easy.py
@@ -3,13 +3,41 @@
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
-
-
+"""
+def my_round(number, ndigits):
+    int_ndigits = int(ndigits)
+    degree = pow(10 ,int(ndigits))
+    mul =  number * degree
+    res = int(mul)
+    ost = mul - res
+    if not (abs(ost) < 0.5):
+        if res > 0: res += 1
+        else: res -= 1
+    return res / degree
+"""
 # Задание-2:
 # Дан шестизначный номер билета. Определить, является ли билет счастливым.
 # Решение реализовать в виде функции.
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
+
+def check(ticket):
+  ticket = str(ticket)
+  t1 = ticket[:3]
+  t2 = ticket[3:]
+  sum1 = 0
+  sum2 = 0
+  for i in t1:
+    sum1 = sum1 + int(i)
+  for i in t2:
+    sum2 = sum2 + int(i)
+    if sum1 == sum2:
+        print ("Счастливый билет!")
+    else:
+        print ("Не счастливый билет!")
+
+
+
 
 
 
--- lesson_03/home_work/hw03_normal.py
+++ lesson_03/home_work/hw03_normal.py
@@ -1,17 +1,59 @@
+import math
 # Задание-1:
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+"""
+def fibonacci(n, m):
+    res = []
+    last = 0
+    next = 1
+    for i in range(m + 1):
+        if i >= n: res.append(next)
+        last, next = next, next + last
+    return res
+
+print(fibonacci(1, 8))
+"""
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
 # Для сортировки используйте любой алгоритм (например пузырьковый).
 # Для решения данной задачи нельзя использовать встроенную функцию и метод sort()
+"""
+def sort_to_max(origin_list):
+    res = origin_list[:]
+    for i in range(1, len(origin_list)):
+        j = i
+        while (res[j] < res[j - 1]) and (j > 0):
+            res[j], res[j - 1] = res[j - 1], res[j]
+            j -= 1
+    return res
 
+print(sort_to_max([1, 5, -10, 4.5, 3, -11, 8, -1, 40]))
+"""
 # Задача-3:
 # Напишите собственную реализацию стандартной функции filter.
 # Разумеется, внутри нельзя использовать саму функцию filter.
+"""
+def my_filter(func, iter):
+    res = []
+    for i in iter:
+        if func(i):
+            res.append(i)
+    return res
 
+def my_iter_filter(func, iter):
+    for i in iter:
+        if func(i):
+            yield i
+
+print((my_filter(lambda x : x > 1, [-1, 2, 3, -4, 0, 5]),[1, 3, 5]))
+"""
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
+
+
+
 
