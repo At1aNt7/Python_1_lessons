b7c8148613057702c58cefc81d1fa7118383b87b
At1aNt7 <At1aNt2007@yandex.ru>
2018-10-31 14:31:56 +0700

    изи сделал все, нормал только 2 задачи, хард не успел...

--- lesson_04/home_work/hw04_normal.py
+++ lesson_04/home_work/hw04_normal.py
@@ -3,7 +3,27 @@
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
+"""
+#с re
 
+import re
+
+foo = "mtMmEZUOmcq"
+low_foo = re.findall(r"([a-z]+)", foo)
+print(low_foo)
+
+#без re
+
+bar = "mtMmEZUOmcq"
+bar = list(bar)
+for letters in bar:
+    if letters.isupper():
+        bar[bar.index(letters)] = "."
+bar = ''.join(bar)
+bar = bar.split(".")
+lower_letters = [letters_lower for letters_lower in bar if letters_lower.islower()]
+print(lower_letters)
+"""
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -12,6 +32,27 @@
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+"""
+import re
+
+letters = "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+pattern = (r"[a-z]{2}([A-Z]+)[A-Z]{2}")
+new_letters = re.findall(pattern, letters)
+print(new_letters)
+"""
+
+#без re (попытался попробовать предыдущим способом, как задаче 1 =) )
+
+bar = "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+bar = list(bar)
+for letters in bar:
+    if letters.islower():
+        bar[bar.index(letters)] = "."
+bar = ''.join(bar)
+bar = bar.split(".")
+isupper_letters = [letters_isupper for letters_isupper in bar if letters_isupper.isupper()]
+print(isupper_letters)
+
 
 
 # Задание-3:
