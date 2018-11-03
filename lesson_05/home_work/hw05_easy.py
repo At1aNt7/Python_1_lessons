22bc371c898f61563c491042e560c6fcb5df116b
At1aNt7 <At1aNt2007@yandex.ru>
2018-11-03 11:30:17 +0700

    Сделал только easy, и то думаю с ошибками...

--- lesson_05/home_work/hw05_easy.py
+++ lesson_05/home_work/hw05_easy.py
@@ -2,13 +2,35 @@
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+import os
+import sys
+import shutil
 
+"""
+try:
+    for i in range(1, 9):
+        os.mkdir("dir_" + str(i))
+except FileExistsError:
+    print("Папка создана")
+
+try:
+    for i in range(1, 9):
+        os.rmdir("dir_" + str(i))
+except FileExistsError:
+    print("Папка удалена")
+"""
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-
-
-
+"""
+list = os.listdir()
+for i in list:
+    print(i)
+"""
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-
+"""
+def script_copy():
+    shutil.copy(sys.argv[0], f'{os.getcwd()}\\copy.py')
+script_copy()
+"""
